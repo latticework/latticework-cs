24845bcfa1fb8ca7f5fdfd86e272ed1340daab7b
gpitte <gulshan.pittie@gmail.com>
2013-07-18 17:10:57 -0400

    adding a dedicated library for packaging having all the library references and dependencies that contributes to latticework.cs

---
+++ source/net45/latticework.cs/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyTitle("latticework.cs")]
+[assembly: AssemblyDescription("latticework.cs")]
+[assembly: ComVisible(false)]
+[assembly: Guid("dad30461-7a8c-42e2-8076-6d03b7621b44")]
