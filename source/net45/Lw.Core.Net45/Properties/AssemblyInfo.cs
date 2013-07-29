436f767602f075c47168414b42881f3542394a9d
gpitte <gulshan.pittie@gmail.com>
2013-07-29 11:21:59 -0400

    fixed lw.core.net45 assembly name and unlinked the globalassembly info file to see if assembly versioning is working for teamcity assembly pathch build feature.

--- source/net45/Lw.Core.Net45/Properties/AssemblyInfo.cs
+++ source/net45/Lw.Core.Net45/Properties/AssemblyInfo.cs
@@ -6,3 +6,19 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Enterprise grade .NET Framework extensions.")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3999f3fc-88f0-4426-b510-5416f2fb82ad")]
+[assembly: AssemblyCompany("latticework")]
+[assembly: AssemblyCopyright("Copyright © Latticework 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Do not update the assembly version except for major revision changes.
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyInformationalVersion("1.0.*")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
