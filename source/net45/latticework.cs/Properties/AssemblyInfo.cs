df52675e7d565dacc2fd4739a989bd36551a6b34
gpitte <gulshan.pittie@gmail.com>
2013-07-29 10:52:59 -0400

    trying assembly version synch

--- source/net45/latticework.cs/Properties/AssemblyInfo.cs
+++ source/net45/latticework.cs/Properties/AssemblyInfo.cs
@@ -7,3 +7,13 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("latticework.cs")]
 [assembly: ComVisible(false)]
 [assembly: Guid("dad30461-7a8c-42e2-8076-6d03b7621b44")]
+[assembly: AssemblyCompany("latticework")]
+[assembly: AssemblyCopyright("Copyright © Latticework 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Do not update the assembly version except for major revision changes.
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyInformationalVersion("1.0.*")]
